4bc35918299f0a2f1fabeac9ff7cfcbdc15c33be
jdmunozPirani <david.gallego@pirani.co>
2025-02-06 16:03:48 -0500

    First commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { loadRemoteModule } from '@angular-architects/module-federation';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'mantenance',
+    loadChildren: () =>
+      loadRemoteModule({
+        type: 'module',
+        remoteEntry: 'http://localhost:4101/remoteEntry.js',
+        exposedModule: './routes',
+      }).then((c) => c.routes),
+  },
+  {
+    path: '**',
+    redirectTo: 'mantenance',
+    pathMatch: 'full',
+  },
+];
